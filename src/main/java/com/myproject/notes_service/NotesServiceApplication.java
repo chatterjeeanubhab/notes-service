9d2d6ce752355673ced71d5bb7f7eaa388664b86
Anubhab Chatterjee <chattopadhyayanubhab@gmail.com>
2026-07-29 19:33:51 +0530

    Implemented JPA auditing

--- src/main/java/com/myproject/notes_service/NotesServiceApplication.java
+++ src/main/java/com/myproject/notes_service/NotesServiceApplication.java
@@ -3,6 +3,7 @@ package com.MyProject.notes_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class NotesServiceApplication {
 
