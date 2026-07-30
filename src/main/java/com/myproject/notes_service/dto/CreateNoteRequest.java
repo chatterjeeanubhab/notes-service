da80514a7718effb1c16770cfd192ab4d0ca3188
Anubhab Chatterjee <chattopadhyayanubhab@gmail.com>
2026-07-30 20:48:32 +0530

    "Added input validation rules"

--- src/main/java/com/myproject/notes_service/dto/CreateNoteRequest.java
+++ src/main/java/com/myproject/notes_service/dto/CreateNoteRequest.java
@@ -3,12 +3,18 @@ package com.myproject.notes_service.dto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 public class CreateNoteRequest {
+    @NotBlank
+    @Size(max = 100)
     private String title;
+    @NotBlank
     private String description;
+    @NotBlank
     private String category;
 }
